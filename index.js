3240ebc9d38ade8253f75b406baab458ae67cd4f
Hayato-Fukaya <108861578+Hayato-Fukaya@users.noreply.github.com>
2022-07-08 15:07:14 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-require('http').createServer((req, res) => res.end('hello')).listen(process.env.PORT || 3000);
+require('http').createServer((req, res) => res.end('<h1>HELLO WORLD</h1>')).listen(process.env.PORT || 3000);https://github.com/Hayato-Fukaya/hayapp/blob/main/index.js
